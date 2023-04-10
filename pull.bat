862254751233189b8ac8dff7f5547c0919a15c43
yaomqlaptop <123456>
2023-04-11 01:11:41 +0800

    none

--- pull.bat
+++ pull.bat
@@ -1,6 +1,5 @@
-git pull origin master
-IF %errorlevel% NEQ 0 GOTO ERROR
-
 :ERROR
 git pull origin master
-IF %errorlevel% NEQ 0 GOTO ERROR
+if errorlevel 1 goto ERROR
+else goto END
+:END
--- push.bat
+++ push.bat
@@ -1,4 +1,8 @@
+:ERROR
 git add .
 git commit -m "none"
 git push origin master
 git pull origin master
+if %errorlevel%==1 (goto ERROR) else (goto END)
+
+:END
