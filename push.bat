7d6901336df409ef3b1a74f5669fe34480b78160
yaomqlaptop <123456>
2023-04-11 20:50:21 +0800

    none

--- push.bat
+++ push.bat
@@ -1,10 +1,10 @@
 :COMMAND1
 git add .
-if %errorlevel% NEQ 0 (goto COMMAND1) else (goto COMMAND2)
+if %errorlevel% NEQ 0 (goto COMMAND2) else (goto COMMAND2)
 
 :COMMAND2
 git commit -m "none"
-if %errorlevel% NEQ 0 (goto COMMAND2) else (goto COMMAND3)
+if %errorlevel% NEQ 0 (goto COMMAND3) else (goto COMMAND3)
 
 :COMMAND3
 git push origin master
